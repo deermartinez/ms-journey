5a2b0fae6a21eacc7d2f8a84803e21f784b1baf1
Destynee Martine <deermartinez8@gmail.com>
2021-02-16 20:32:18 -0700

    button beg

--- journey/src/Components/button.js
+++ journey/src/Components/button.js
@@ -1,1 +1,21 @@
-import React from 'react'
+import React from 'react';
+import '../Styles/button.css';
+import {Link} from 'react-router-dom';
+
+
+
+const STYLES = ['btn-primary', 'btn-outline'];
+
+const SIZES = ['btn--medium', 'btn--large'];
+
+export const Button = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize
+}) => {
+    const checkButtonStyle = StyLES.includes(buttonStyle) ?
+    buttonStyle : STYLES[0]
+
+};
